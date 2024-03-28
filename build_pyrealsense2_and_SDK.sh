7729e5b0c2173a7f390ca063a7867acbbc9599a0
lesreaper <rich@tommyland.org>
2024-03-28 00:10:03 -0400

    More fixes for Orin

--- build_pyrealsense2_and_SDK.sh
+++ build_pyrealsense2_and_SDK.sh
@@ -8,7 +8,11 @@
 cd ~/Repos
 git clone https://github.com/IntelRealSense/librealsense.git
 cd librealsense && mkdir build && cd build
-sudo apt-get -y install python3 python3-dev libssl-dev libxinerama-dev libxcursor-dev libcanberra-gtk-module libcanberra-gtk3-module
+sudo apt-get -y install python3-dev libssl-dev libxinerama-dev libxcursor-dev libcanberra-gtk-module libcanberra-gtk3-module
+sudo apt-get -y install libusb-1.0-0-dev pkg-config
+sudo apt-get -y install build-essential cmake cmake-curses-gui
+sudo apt-get -y install libgtk-3-dev libglfw3-dev libgl1-mesa-dev libglu1-mesa-dev
+
 echo "[INFO] pyrealsense2 bindings build has been started!"
 sleep 2
 # Give the CUDA path to CMake
@@ -24,7 +28,7 @@ sudo make -j$(($(nproc)-1)) && sudo make install
 echo 'PYTHONPATH="/usr/local/lib:/usr/local/lib/python3.10/pyrealsense2:$PYTHONPATH"' >> ~/.bashrc
 echo 'export PYTHONPATH' >> ~/.bashrc
 
-sudo cp ~/librealsense/config/99-realsense-libusb.rules /etc/udev/rules.d/ && sudo udevadm control --reload-rules && udevadm trigger
+sudo cp ~/Repos/librealsense/config/99-realsense-libusb.rules /etc/udev/rules.d/ && sudo udevadm control --reload-rules && udevadm trigger
 
 echo "[INFO] pyrealsense2 and RealSense SDK are ready to use!"
 echo '[INFO] pyrealsense2 can only be used with "python3" not with "python"!'
