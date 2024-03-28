d5e99caabce0ef85a49c6096fe390bcf019c8e70
lesreaper <rich@tommyland.org>
2024-03-27 21:37:27 -0400

    Updates for Orin Nano

--- build_pyrealsense2_and_SDK.sh
+++ build_pyrealsense2_and_SDK.sh
@@ -5,7 +5,7 @@
 # 	      https://github.com/IntelRealSense/librealsense/issues/6964
 #	      https://www.youtube.com/watch?v=EeT-pzM8n-o
 
-cd ~
+cd ~/Repos
 git clone https://github.com/IntelRealSense/librealsense.git
 cd librealsense && mkdir build && cd build
 sudo apt-get -y install python3 python3-dev libssl-dev libxinerama-dev libxcursor-dev libcanberra-gtk-module libcanberra-gtk3-module
@@ -21,10 +21,12 @@ echo "[INFO] Building is starting, it will take a long time like half an hour or
 sleep 2
 sudo make -j$(($(nproc)-1)) && sudo make install
 
-echo 'PYTHONPATH="/usr/local/lib:/usr/local/lib/python3.6/pyrealsense2:$PYTHONPATH"' >> ~/.bashrc
+echo 'PYTHONPATH="/usr/local/lib:/usr/local/lib/python3.10/pyrealsense2:$PYTHONPATH"' >> ~/.bashrc
 echo 'export PYTHONPATH' >> ~/.bashrc
 
 sudo cp ~/librealsense/config/99-realsense-libusb.rules /etc/udev/rules.d/ && sudo udevadm control --reload-rules && udevadm trigger
 
 echo "[INFO] pyrealsense2 and RealSense SDK are ready to use!"
 echo '[INFO] pyrealsense2 can only be used with "python3" not with "python"!'
+
+echo '[INFO] Run your scripts update for the kernel in the LibRealSense library!'
